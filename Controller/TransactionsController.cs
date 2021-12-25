18c707dd3539240de6192599be9febfe0c7c99be
HadiKhai <hadikhairallah@outlook.com>
2021-12-25 22:31:39 +0200

    feat: controller

--- Controller/TransactionsController.cs
+++ Controller/TransactionsController.cs
@@ -1,64 +1,44 @@
 using System;
 using System.Collections.Generic;
 using AutoMapper;
-using UserManagement.DTO;
-using UserManagement.Models;
-using UserManagement.Services;
+using LauToken.DTO;
+using LauToken.Models;
+using LauToken.Repository;
+using LauToken.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 
-namespace UserManagement.Controller
+namespace LauToken.Controller
 {
-    [Route("api/users")]
+    [Route("api/transactions")]
     [ApiController]
-    public class UsersController: ControllerBase
+    public class TransactionsController: ControllerBase
     {
-        private readonly IUserService _service;
+        private readonly ITransactionService _service;
         private readonly IMapper _mapper;
 
-        public UsersController(IUserService service, IMapper mapper)
+        public TransactionsController(ITransactionService service, IMapper mapper)
         {
             _service = service;
             _mapper = mapper;
         }
         
-        [HttpGet]
-        public ActionResult <IEnumerable<User>> GetAllUsers()
+        [HttpGet("{address}",Name = "GetTransactionsByAddress")]
+        public ActionResult <IEnumerable<TransactionReadDTO>> GetTransactionsByAddress(string address)
         {
-            var users = _service.GetAllUsers();
+            var transactions = _service.GetTransactionsByAddress(address);
 
-            return Ok(users);
+            return Ok(transactions);
         }
         
-        [HttpGet("filtered", Name="GetAllFilteredUsers")]
-        public ActionResult <IEnumerable<UserReadDTO>> GetAllFilteredUsers()
-        {
-            var users = _service.GetAllUsers();
-
-            return Ok(_mapper.Map<IEnumerable<UserReadDTO>>(users));
-        }
-        
-        [HttpGet("{id}", Name="GetUserById")]
-        public ActionResult <User> GetUserById(Guid id)
+        [HttpPost("create")]
+        public ActionResult <Transaction> CreateTransaction(TransactionCreateDTO transaction)
         {
             try
             {
-                var user = _service.GetUserById(id);
-                return Ok(user);
-            }
-            catch (Exception e) {
-                Console.WriteLine(e);
-                return Problem(e.GetBaseException().ToString());
-            }
-        }
-        [HttpGet("filtered/{id}")]
-        public ActionResult <User> GetUserFilteredById(Guid id)
-        {
-            try
-            {
-                var user = _service.GetUserById(id);
-                return Ok(_mapper.Map<UserReadDTO>(user));
+                var tx = _service.CreateTransaction(_mapper.Map<Transaction>(transaction));
+                return CreatedAtRoute("GetTransactionsByAddress", new { Address = tx.Sender }, tx);
             }
             catch (Exception e) {
                 Console.WriteLine(e);
@@ -66,62 +46,5 @@ namespace UserManagement.Controller
             }
         }
         
-        [HttpPost]
-        public ActionResult <UserReadDTO> CreateUser(UserCreateDTO userCreateDto)
-        {
-            try
-            {
-                var userModel = _mapper.Map<User>(userCreateDto);
-                var userReadDto = _mapper.Map<UserReadDTO>(_service.CreateUser(userModel));
-                return CreatedAtRoute("GetUserById", new { Id = userReadDto.UserId }, userReadDto);
-            }
-            catch (Exception e) {
-                Console.WriteLine(e);
-                return Problem(e.GetBaseException().ToString());
-            }
-        }
-        
-        [HttpPut("{id}")]
-        public ActionResult UpdateUser(Guid id, UserUpdateDTO userUpdateDto)
-        {
-            try
-            {
-                _service.UpdateUser(id,userUpdateDto);
-                return NoContent();
-            }
-            catch (Exception e) {
-                Console.WriteLine(e);
-                return Problem(e.GetBaseException().ToString());
-            }
-        }
-
-        [HttpPatch("{id}")]
-        public ActionResult PartialUserUpdate(Guid id, JsonPatchDocument<UserUpdateDTO> patchDoc)
-        {
-            try
-            {
-                _service.PatchUser(id,patchDoc,ModelState);
-                return NoContent();
-            }
-            catch (Exception e) {
-                Console.WriteLine(e);
-                return Problem(e.GetBaseException().ToString());
-            }
-        }
-
-        //DELETE api/Users/{id}
-        [HttpDelete("{id}")]
-        public ActionResult DeletedUser(Guid id) {
-            try
-            {
-                _service.DeletedUserById(id);
-                return NoContent();
-            }
-            catch (Exception e) {
-                Console.WriteLine(e);
-                return Problem(e.GetBaseException().ToString());
-            }
-        }
-
     }
 }
